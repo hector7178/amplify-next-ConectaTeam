49cf05e49f63eff97d161c052a81920067a7193e
hector7178 <hjgrimanmarcano@gmail.com>
2024-11-15 11:23:26 -0400

    PP and TOC

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,3 +4,50 @@ import { defineAuth } from "@aws-amplify/backend";
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "BiENVENIDO A CONECTATEAM!",
+      verificationEmailBody: (createCode) => `Este es el codigo para verificar tu cuenta:\n \n ${createCode()}`,
+      userInvitation: {
+           emailSubject: "Bienvenido a conectaTeam!",
+      emailBody: (user, code) =>
+             `Estamos felices que formes parte de nosotros, ya puedes iniciar sesión:\n\n usuario: ${user()} \n contraseña: ${code()}`, 
+          },
+     },
+  },
+  userAttributes: {
+    birthdate: {
+      mutable: true,
+      required: false,
+    },
+    nickname:{
+      mutable: true,
+      required: true,
+    },
+    phoneNumber:{
+      mutable: true,
+      required: true,
+    },
+    "custom:rol":{
+      dataType: "String",
+      mutable: true,
+      maxLen: 16,
+      minLen: 1,
+    },
+    "custom:company":{
+      dataType:"String",
+      mutable:true,
+      maxLen:16,
+      minLen:1
+    },
+    "custom:country":{
+      dataType:"String",
+      mutable:true,
+      maxLen:16,
+      minLen:1
+    },
+
+  }
+});
